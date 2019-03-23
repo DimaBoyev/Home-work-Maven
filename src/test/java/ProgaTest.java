02f26ec9e39795124c7c196f9b8718262f5c36aa
DimaBoyev <dmitriy.boyev@gmail.com>
2019-03-23 10:06:29 +0200

    project updated

--- src/test/java/ProgaTest.java
+++ src/test/java/ProgaTest.java
@@ -1,3 +1,5 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -5,46 +7,59 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ProgaTest {
 
+    private String url;
+    private WebDriver driver;
+
+    @Before
+
+        public void beforeMethod(){
+        driver = new ChromeDriver();
+        url = "http://37.59.228.229:3000/";
+        driver.get(url);
+    }
+
+
+
     @Test //создать юзера с ролью "Student"
     public void testProgram() {
-        WebDriver drovulichka = new ChromeDriver();
-        drovulichka.get("http://37.59.228.229:3000/");
-        drovulichka.findElement(By.xpath("//i[@class='mdi-content-add']")).click();
-        drovulichka.findElement(By.xpath("//input[@id='icon_prefix']")).clear();
-        drovulichka.findElement(By.xpath("//input[@id='icon_prefix']")).sendKeys("Spider Man");
-        drovulichka.findElement(By.xpath("//input[@id='icon_telephone']")).clear();
-        drovulichka.findElement(By.xpath("//input[@id='icon_telephone']")).sendKeys("380503911544");
-        drovulichka.findElement(By.cssSelector("input.select-dropdown")).click();
-        drovulichka.findElement(By.xpath("/html/body/div/div[2]/form/div[2]/div/ul/li[3]/span")).click(); //создать юзера с ролью "Student"
-        drovulichka.findElement(By.cssSelector("div.col a.save-btn.waves-effect.waves-light.btn")).click();
+        driver.findElement(By.xpath("//i[@class='mdi-content-add']")).click();
+        driver.findElement(By.xpath("//input[@id='icon_prefix']")).clear();
+        driver.findElement(By.xpath("//input[@id='icon_prefix']")).sendKeys("Spider Man");
+        driver.findElement(By.xpath("//input[@id='icon_telephone']")).clear();
+        driver.findElement(By.xpath("//input[@id='icon_telephone']")).sendKeys("380503911544");
+        driver.findElement(By.cssSelector("input.select-dropdown")).click();
+        driver.findElement(By.xpath("/html/body/div/div[2]/form/div[2]/div/ul/li[3]/span")).click();
+        driver.findElement(By.cssSelector("div.col a.save-btn.waves-effect.waves-light.btn")).click();
 
     }
 
     @Test //создать юзера с ролью "Administrator"
     public void testProgram1() {
-        WebDriver drovishka = new ChromeDriver();
-        drovishka.get("http://37.59.228.229:3000/");
-        drovishka.findElement(By.xpath("//i[@class='mdi-content-add']")).click();
-        drovishka.findElement(By.xpath("//input[@id='icon_prefix']")).clear();
-        drovishka.findElement(By.xpath("//input[@id='icon_prefix']")).sendKeys("Deadpool");
-        drovishka.findElement(By.xpath("//input[@id='icon_telephone']")).clear();
-        drovishka.findElement(By.xpath("//input[@id='icon_telephone']")).sendKeys("380676973878");
-        drovishka.findElement(By.cssSelector("input.select-dropdown")).click();
-        drovishka.findElement(By.xpath("/html/body/div/div[2]/form/div[2]/div/ul/li[1]/span")).click();
-        drovishka.findElement(By.cssSelector("div.col a.save-btn.waves-effect.waves-light.btn")).click();
+        driver.findElement(By.xpath("//i[@class='mdi-content-add']")).click();
+        driver.findElement(By.xpath("//input[@id='icon_prefix']")).clear();
+        driver.findElement(By.xpath("//input[@id='icon_prefix']")).sendKeys("Deadpool");
+        driver.findElement(By.xpath("//input[@id='icon_telephone']")).clear();
+        driver.findElement(By.xpath("//input[@id='icon_telephone']")).sendKeys("380676973878");
+        driver.findElement(By.cssSelector("input.select-dropdown")).click();
+        driver.findElement(By.xpath("/html/body/div/div[2]/form/div[2]/div/ul/li[1]/span")).click();
+        driver.findElement(By.cssSelector("div.col a.save-btn.waves-effect.waves-light.btn")).click();
     }
-    @Test //создать юзера с ролью "Support" и выберем локацию "Одесса"
+    @Test //создать юзера с ролью "Support"
     public void testProgram3() {
-        WebDriver drova = new ChromeDriver();
-        drova.get("http://37.59.228.229:3000/");
-        drova.findElement(By.xpath("//i[@class='mdi-content-add']")).click();
-        drova.findElement(By.xpath("//input[@id='icon_prefix']")).clear();
-        drova.findElement(By.xpath("//input[@id='icon_prefix']")).sendKeys("Iron Man");
-        drova.findElement(By.xpath("//input[@id='icon_telephone']")).clear();
-        drova.findElement(By.xpath("//input[@id='icon_telephone']")).sendKeys("380634623804");
-        drova.findElement(By.cssSelector("input.select-dropdown")).click();
-        drova.findElement(By.xpath("/html/body/div/div[2]/form/div[2]/div/ul/li[2]/span")).click();
-        drova.findElement(By.cssSelector("div.col a.save-btn.waves-effect.waves-light.btn")).click();
+        driver.findElement(By.xpath("//i[@class='mdi-content-add']")).click();
+        driver.findElement(By.xpath("//input[@id='icon_prefix']")).clear();
+        driver.findElement(By.xpath("//input[@id='icon_prefix']")).sendKeys("Iron Man");
+        driver.findElement(By.xpath("//input[@id='icon_telephone']")).clear();
+        driver.findElement(By.xpath("//input[@id='icon_telephone']")).sendKeys("380634623804");
+        driver.findElement(By.cssSelector("input.select-dropdown")).click();
+        driver.findElement(By.xpath("/html/body/div/div[2]/form/div[2]/div/ul/li[2]/span")).click();
+        driver.findElement(By.cssSelector("div.col a.save-btn.waves-effect.waves-light.btn")).click();
 
+    }
+
+    @After
+
+    public void afterMethod(){
+        driver.quit();
     }
 }
